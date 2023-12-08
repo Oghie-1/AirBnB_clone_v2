75b61cfa9e22e84064be76eaf6fdb596da8dd0f6
afro <aigbemanuel@gmail.com>
2023-12-07 18:50:18 -0800

    0-setup refix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,12 +8,5 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-
-# Append the new location block to the Nginx configuration
-sudo tee -a /etc/nginx/sites-available/default <<EOF
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-	}
-EOF
-
-sudo service nginx restart
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
