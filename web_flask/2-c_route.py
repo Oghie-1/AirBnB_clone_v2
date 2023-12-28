f2dbcd0654f9718cff97f3511dcd41fdfb234056
afromena <aigbemanuel@gmail.com>
2023-12-28 04:48:22 -0800

    added files

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,15 +17,17 @@ def hello_hbnb():
 	"""Returns 'Hello HBNB!'"""
 	return "Hello HBNB!"
 
-
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
 	"""Returns 'HBNB' """
 	return "HBNB"
 
+@app.route("/c/<text>", strict_slashes=Flase)
 def c_text(text):
 	"""Retruns 'C' followed by the value of <text>"""
 	text = text.replace("_"," ")
 	return "C {}".format(text)
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Starts flask web app
+listening on 0.0.0.0, port 5000
+routes: '/': displays "Hello HBNB!"
+	'/hbnb': displays "HBNB"
+	'/c/<text>': displays 'C' followed by the value of the text variable
+	'/python/(text)': displays 'Python' followed by the value of the text variable
+	'/number/<n>': displays 'n is a number' only if n is an interger"""
+
+
